5d82f81dc72871fea7f6bd3a3428db148df031f1
Toan Tran <huutrantoan@gmail.com>
2023-08-18 09:47:23 +0700

    feat: automate postgres restart, prisma migrate, auth logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return 'signup';
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+      throw error;
+    }
   }
 
-  signin() {
-    return 'signin';
+  async signin(dto: AuthDto) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    if (user) {
+      const pwMatches = await argon.verify(
+        user.hash,
+        dto.password,
+      );
+
+      if (!pwMatches)
+        throw new ForbiddenException(
+          'Credentials incorrect',
+        );
+    } else {
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+    }
+
+    // return this.signToken(user.id, user.email);
   }
+
+  // async signToken(
+  //   userId: number,
+  //   email: string,
+  // ): Promise<{ access_token: string }> {
+  //   const payload = {
+  //     sub: userId,
+  //     email,
+  //   };
+  //   const secret = this.config.get('JWT_SECRET');
+
+  //   const token = await this.jwt.signAsync(
+  //     payload,
+  //     {
+  //       expiresIn: '15m',
+  //       secret: secret,
+  //     },
+  //   );
+
+  //   return {
+  //     access_token: token,
+  //   };
+  // }
 }
