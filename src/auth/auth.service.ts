3f0f43dd14aed7c1f2e3889c1c899a96cef41674
Toan Tran <huutrantoan@gmail.com>
2023-08-24 22:28:25 +0700

    feat: authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,14 +2,20 @@ import {
   ForbiddenException,
   Injectable,
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     try {
       const hash = await argon.hash(dto.password);
@@ -63,29 +69,29 @@ export class AuthService {
       );
     }
 
-    // return this.signToken(user.id, user.email);
+    return this.signToken(user.id, user.email);
   }
 
-  // async signToken(
-  //   userId: number,
-  //   email: string,
-  // ): Promise<{ access_token: string }> {
-  //   const payload = {
-  //     sub: userId,
-  //     email,
-  //   };
-  //   const secret = this.config.get('JWT_SECRET');
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
 
-  //   const token = await this.jwt.signAsync(
-  //     payload,
-  //     {
-  //       expiresIn: '15m',
-  //       secret: secret,
-  //     },
-  //   );
+    const token = await this.jwt.signAsync(
+      payload,
+      {
+        expiresIn: '15m',
+        secret,
+      },
+    );
 
-  //   return {
-  //     access_token: token,
-  //   };
-  // }
+    return {
+      access_token: token,
+    };
+  }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtGuard } from 'src/auth/guard';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@GetUser('') user: User) {
+    return user;
+  }
+}
